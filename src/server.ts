ee1097792f0de7daf2d42e05eecfb4102076662c
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-12-31 18:12:23 -0400

    feat: add cors to allow cross-origin requests

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'express-async-errors'
 import express from 'express'
+import cors from 'cors'
 import { createConnection } from 'typeorm'
 
 import globalErrors from './middlewares/globalErrors'
@@ -10,6 +11,7 @@ createConnection()
     const app = express()
     const port = 3333
 
+    app.use(cors())
     app.use(express.json())
     app.use(routes)
     app.use(globalErrors)
