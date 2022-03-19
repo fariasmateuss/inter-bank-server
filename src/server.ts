7466f9ed0780f0d4a5e29520200e2ac25730d2ee
Wesley Oliveira <wesleyoliveiradeveloper@gmail.com>
2022-03-19 20:54:22 -0300

    refactor: restructure server core using classes

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,12 @@
 import 'express-async-errors'
 import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
 
-import './database'
-import globalErrors from './middlewares/globalErrors'
-import routes from './routes'
+import app from './app'
 
-const app = express()
-const port = 3333
+const appPort = process.env.PORT || 3333
 
-app.use(cors())
-app.use(express.json())
-app.use(routes)
-app.use(globalErrors)
+function bootstrap() {
+  app.listen(appPort)
+}
 
-app.listen(port, () => console.log(`Server is listening on ${port}`))
+bootstrap()
